4eebc801b35106530d170ee4d7b2c9edb8e56963
RyanWolfen7 <wolfen7lada@gmail.com>
2018-11-11 21:53:33 +0000

    added game class'

--- src/scoreCard.js
+++ src/scoreCard.js
@@ -1,1 +1,28 @@
+function Players {
+  var players = [];
+}
 
+
+Players.prototype.addPlayer(name) = function () {
+  players.push(name);
+};
+
+Players.prototype.deletePlayer(name) = function() {
+  players.pop(name);
+};
+
+Players.prototype.currentPlayer() = function() {
+  return players;
+};
+
+Players.prototype.nextPlayer() = function() {
+  players.next();
+};
+
+Array.prototype.next = function() {
+    return this[++this.current];
+};
+Array.prototype.prev = function() {
+    return this[--this.current];
+};
+Array.prototype.current = 0;
